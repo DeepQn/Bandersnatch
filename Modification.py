de152336268c20451a025521628391ac78f70ec4
DeepQn <48483263+DeepQn@users.noreply.github.com>
2019-07-30 16:17:59 +0530

    Update Modification.py

--- Modification.py
+++ Modification.py
@@ -34,9 +34,9 @@ def convert(s):
 # function for finding the intervals between which the traffic peaks lie
 # Returns the range where the number of get request are maximum.
 
-sample_file = 'Bandersnatch\\Test1.pcapng'
-source_ip = '192.168.7.32'
-dest_ip = '34.250.41.147'
+sample_file = 'Test1/Test1.pcapng'
+source_ip = '192.168.1.238'
+dest_ip = '54.194.132.188'
 
 # initial range (ssl record length) of cl2 json files
 initial_range_cl2_noh = 4500
@@ -45,40 +45,36 @@ initial_range_cl2_noh = 4500
 final_range_cl2_noh = 7000
 
 # expected time of arrival of cl2 json for the first question
-time_of_1st_choice_cl2 = 163
-# finding cl2 json files which are sent from client
-src_addr_cl2 = source_ip
+time_of_1st_choice_cl2 = 158
+
 # capturing application data files within the initial_range_cl2_noh and final_range_cl2_noh
 cl2_json_capture = pyshark.FileCapture(sample_file,
-                                       display_filter="(ssl.record.content_type==23 && ssl.record.length > {} && ssl.record.length < {} && frame.time_relative > {} && ip.src=={})".format(
+                                       display_filter="(ssl.record.content_type==23 && ssl.record.length > {} && ssl.record.length < {} && frame.time_relative > {} && ip.src=={} && ip.dst=={})".format(
                                            initial_range_cl2_noh, final_range_cl2_noh, time_of_1st_choice_cl2,
-                                           src_addr_cl2))
+                                           source_ip, dest_ip))
 
 # initial range (ssl record length) of server-site (Type1 and Type2) json files
-initial_range_server_noh = 2026
+initial_range_server_noh = 2025
 
 # final range (ssl record length) of server-site (Type1 and Type2) json files
 final_range_server_noh = 2038
 
 # expected time of arrival of the first question
-time_of_1st_choice_server = 163
-
-# finding server-site (Type1 and Type2) json files which are sent to client
-dst_addr_server = source_ip
+time_of_1st_choice_server = 158
 
 # capturing application data files within the initial_range_server_noh and final_range_server_noh
 server_json_capture = pyshark.FileCapture(sample_file,
-                                          display_filter="(ssl.record.content_type==23 && ip.dst=={} && frame.time_relative > {} && ssl.record.length > {} && ssl.record.length < {})".format(
-                                              dst_addr_server, time_of_1st_choice_server, initial_range_server_noh,
+                                          display_filter="(ssl.record.content_type==23 && ip.dst=={} && ip.src=={} && frame.time_relative > {} && ssl.record.length > {} && ssl.record.length < {})".format(
+                                              source_ip, dest_ip, time_of_1st_choice_server, initial_range_server_noh,
                                               final_range_server_noh))
 
 # initializing global variables
 global next_pckt_h, next_type2_pckt, non_default_flag
 
 for server_json in server_json_capture:
-    if 2036 <= float(server_json.ssl.record_length) <= 2038:
+    if 2035 <= float(server_json.ssl.record_length) <= 2038:
         server_json_time = float(server_json.frame_info.time_relative)
-        print("Type1 Json", server_json_time)
+        print("Type1 Json:", server_json_time)
         previous_flag = False
 
         for pckt in cl2_json_capture:
@@ -88,7 +84,7 @@ for server_json in server_json_capture:
                     pckt.frame_info.time_relative) < server_json_time - 10 and initial_range_cl2_noh < int(
                     pckt.ssl.record_length) < final_range_cl2_noh:
                 previous_flag = True
-                print("Cl2 Json", pckt.frame_info.time_relative)
+                print("Question:", pckt.frame_info.time_relative)
 
                 # extracting relative time
                 current_pckt_time = float(pckt.frame_info.time_relative)
@@ -110,7 +106,7 @@ for server_json in server_json_capture:
                                 next_type_pckt.frame_info.time_relative) <= current_pckt_time + 20:
 
                             # check ssl record length to identify the packet as Type1 json
-                            if 2027 <= float(next_type_pckt.ssl.record_length) <= 2037:
+                            if 2026 <= float(next_type_pckt.ssl.record_length) <= 2037:
                                 # if found any Type1 json then auto-click occurred
                                 print("Auto clicked-> Frame No: ", pckt.frame_info.number, end="\n\n")
 
@@ -125,19 +121,21 @@ for server_json in server_json_capture:
                         # if any Type2 json is found then non-default choice is selected
                         if current_pckt_time + 10 <= float(
                                 next_type2_pckt.frame_info.time_relative) <= current_pckt_time + 20:
-                            if 2027 <= float(next_type2_pckt.ssl.record_length) <= 2030:
+                            if 2026 <= float(next_type2_pckt.ssl.record_length) <= 2030:
                                 print("Non default choice is selected", end="\n\n")
                                 non_default_flag = True
                                 break
 
                     # if only Type1 json file is captured then default choice is selected
                     if not non_default_flag:
-                        for next_type1_pckt in server_json_capture:
-                            if current_pckt_time + 10 <= float(
-                                    next_type1_pckt.frame_info.time_relative) <= current_pckt_time + 20:
-                                if 2036 <= float(next_type1_pckt.ssl.record_length) <= 2038:
-                                    print("Default choice is selected", end="\n\n")
-                                    break
+                        print("Default choice is selected", end="\n\n")
+                        break
+                        # for next_type1_pckt in server_json_capture:
+                        #     if current_pckt_time + 10 <= float(
+                        #             next_type1_pckt.frame_info.time_relative) <= current_pckt_time + 20:
+                        #         if 2035 <= float(next_type1_pckt.ssl.record_length) <= 2038:
+                        #             print("Default choice is selected", end="\n\n")
+                        #             break
 
         if not previous_flag:
-            print("Previous choice was default")
+            print("Previous choice was default", end="\n\n")
